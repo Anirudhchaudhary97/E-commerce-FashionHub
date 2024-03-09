f132d03ae6a82ed3c90d035d75eb26320c1ac59a
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-03-09 20:32:20 +0545

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../Assest/logo.png";
+import cart_icon from "../Assest/cart_icon.png";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>
+          <span>Fashion</span>Hub
+        </p>
+      </div>
+
+      <ul className="nav-menu">
+        <Link to="/" className="text-link">
+          <li
+            onClick={() => {
+              setMenu("shop");
+            }}
+          >
+            Shop{menu === "shop" ? <hr /> : <></>}
+          </li>
+        </Link>
+        <Link to="/men" className="text-link">
+          <li
+            onClick={() => {
+              setMenu("men");
+            }}
+          >
+            Men{menu === "men" ? <hr /> : <></>}
+          </li>
+        </Link>
+        <Link to="/women" className="text-link">
+          <li
+            onClick={() => {
+              setMenu("women");
+            }}
+          >
+            Women {menu === "women" ? <hr /> : <></>}
+          </li>
+        </Link>
+        <Link to="kids" className="text-link">
+          <li
+            onClick={() => {
+              setMenu("kids");
+            }}
+          >
+            Kids{menu === "kids" ? <hr /> : <></>}
+          </li>
+        </Link>
+      </ul>
+      <div className="nav-login-cart">
+        <Link className="text-link" to="/login">
+          <button>Login</button>
+        </Link>
+        <Link className="text-link" to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/LoginSignup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginSignup = () => {
+  return (
+    <div>
+       loginsugnup
+    </div>
+  )
+}
+
+export default LoginSignup
